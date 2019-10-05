9733dec86fba7973f1c8fac6b52f484c0fca07b7
Matt Ryan <mryan@carleton.edu>
2019-10-04 22:49:28 -0500

    Basic CSS and JS

--- js/gutentolerance.js
+++ js/gutentolerance.js
@@ -1,1 +1,14 @@
 console.log('gutentolerance loaded');
+
+/*
+ * SIDEBAR CUSTOMIZATION
+ * Customize the editor sidebar. Reference available methods in the Gutenberg Handbook...
+ * https://wordpress.org/gutenberg/handbook/designers-developers/developers/data/data-core-edit-post/
+ */
+
+if (typeof wp.data !== 'undefined' && typeof wp.data.dispatch('core/edit-post') !== 'undefined') {
+	const { toggleEditorPanelOpened } = wp.data.dispatch('core/edit-post');
+
+	// Begin with the Page Attributes panel open
+	toggleEditorPanelOpened('page-attributes');
+}
