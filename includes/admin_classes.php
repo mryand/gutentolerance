0d60d587128a48e329af1ca1729b49ecfae7af10
Matt Ryan <mryan@carleton.edu>
2019-11-18 08:50:29 -0600

    Fixed bug

--- includes/admin_classes.php
+++ includes/admin_classes.php
@@ -3,11 +3,8 @@
 function gutentolerance_admin_body_class($classes) {
     $add_classes = array('gutentolerance-enabled');
     $options = get_option( 'gutentolerance_options' );
-    echo '<pre>';
-    print_r($options);
-    echo '</pre>';
-    if(isset($options['field-suppress-preview'])) {
-        $add_classes[] = 'gutentolerance-suppress-preview--'.$options['field-suppress-preview'];
+    if(isset($options['gutentolerance_field_suppress_preview'])) {
+        $add_classes[] = 'gutentolerance-suppress-preview--'.$options['gutentolerance_field_suppress_preview'];
     } else {
         $add_classes[] = 'gutentolerance-suppress-preview--yes';
     }
